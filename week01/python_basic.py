4388dd6484ca3c37cd3873acde7c760c991af791
방경민 <kangmin2325@gmail.com>
2026-03-11 20:29:03 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -34,3 +34,34 @@ print(a[0])
 print(a[4])
 print(a[-1])
 print(a[-2])
+
+#문자열 슬라이싱 
+b = a[0] + a[1] +a[2]
+print(b)
+
+print(a[4:6])
+print(a[19:])
+print(a[:3])
+print(a[7:-11])
+
+#문자 개수 계산
+a = "Python"
+print(a.count('p'))
+
+#문자 위치 확인 
+print(a.find('y'))
+print(a.find('p'))
+print(a.index('y'))
+# print(a.index('p')) # 오류 발생
+
+#문자 삽입 
+b = ","
+c =b.join('Abcd')
+print(c)
+
+#대소문자 변환
+print(a.upper())
+print(a.lower())
+
+# 공백 제거
+d = "         "
