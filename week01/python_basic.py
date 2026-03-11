3e7a756bbac0b14707b3f49a70451371cbda9516
방경민 <kangmin2325@gmail.com>
2026-03-11 20:24:03 +0900

    Updated Changes

---
+++ week01/python_basic.py
@@ -0,0 +1,27 @@
+a = 3
+b = 4
+
+#지수승
+print(a ** b)
+print(a ** 3)
+
+#나머지 연산
+print(a % b)
+print(7 % 3)
+
+#나눗셈 몫 구하기 
+print(a // b)
+print(7 // 3)
+
+s1 = 'Hello Python'
+print(s1)
+
+s3 = '''Hello
+Python'''
+print(s3)
+
+head = "Python"
+tail = "is fun"
+print(head +tail)
+
+#문자열 곱하기 
