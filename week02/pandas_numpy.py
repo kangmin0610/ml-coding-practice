57eccb1a9185e38e6e7bafd510e0053c4ae9ec18
방경민 <kangmin2325@gmail.com>
2026-03-16 20:12:50 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -149,5 +149,12 @@ average_df = df[['name', 'average']]
 print(average_df)
 
 # 2.2 넘파이
+import numpy as np
 
-print(np.array)
+print(np.array([1,2,3]))
+
+print(np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]]))
+
+print(np.array([[]]))
