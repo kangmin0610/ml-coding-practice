ea345ec75345024ada52c328213ca085e39df9f1
방경민 <kangmin2325@gmail.com>
2026-03-16 19:57:50 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -76,10 +76,16 @@ print(df.head())
 
 # 필터링 조건 설정
 condition = (
-    (df['comment_lenght'] >= 100) &       # 댓글 길이 100자 이상
-    (df['likes'] >= 20) &                 # 좋아요 20개 이상
-    (~df['is_spam']) &                    # 스팸 댓글이 아니어야 함
-    (df['has_image'])                     # 이미지가 포함된 댓글이어야 함
+  (df['comment_lenght'] >= 100) &       # 댓글 길이 100자 이상
+  (df['likes'] >= 20) &                 # 좋아요 20개 이상
+  (~df['is_spam']) &                    # 스팸 댓글이 아니어야 함
+  (df['has_image'])                     # 이미지가 포함된 댓글이어야 함
 )
 
-# 조건을 만족하는
+# 조건을 만족하는 행들 필터링
+winner_df = df[condition]
+print(winner_df)
+
+print(sample_df.reset_index())
+
+print(sample_df.reset_index(drop=True))
