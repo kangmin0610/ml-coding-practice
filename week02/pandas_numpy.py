21abba32d9a06e40d3c2082f1c6cfdf676efa17f
방경민 <kangmin2325@gmail.com>
2026-03-16 20:28:20 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -224,4 +224,5 @@ print(a.min())    # 모든 요소 중 최솟값
 print(a.max())    # 모든 요소 중 최댓값
 
 print(a.max(axis=1))
-print(a.cumsum()) 
+print(a.cumsum())        # 모든 요소의 누적합
+print(a.argmax())        # 모든 요소 중 최댓값의 인덱스
