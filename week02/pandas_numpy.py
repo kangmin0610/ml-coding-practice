09292d38bce17193d5574ddb96145f28b4cc6aee
방경민 <kangmin2325@gmail.com>
2026-03-16 19:51:50 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -58,4 +58,13 @@ print(less2015.head())
 
 print((netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show'))
 
-more2015_tv = netflix[(netflix['release_year'] > 2015) & ]
+more2015_tv = netflix[(netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show')]
+print(more2015_tv.head())
+
+more2015_or_tv = netflix[(netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show')]
+print(more2015_or_tv.head())
+
+data = {
+    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hannah'],
+    
+}
