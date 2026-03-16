3f0979e55dec3a93ba0d53cc17e71577501ae2bc
방경민 <kangmin2325@gmail.com>
2026-03-16 19:41:20 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -25,11 +25,11 @@ print(sample_df.head())
 
 print(sample_df.head())
 
-# print(sample_df['var_1', 'var_2'])    # [ ] 안에는 하나의 값만  들어갈 수 있음
-print(sample_df[['var_1', 'var_4']])    # [ [] ]를 사용하면 [] 가 하나의 값으로 인식됨
+# print(sample_df['var_1', 'var_2'])   # [ ] 안에는 하나의 값만  들어갈 수 있음
+print(sample_df[['var_1', 'var_4']])   # [ [] ]를 사용하면 [] 가 하나의 값으로 인식됨
 
 # loc 는 location의 앞글자
-print(sample_df.loc['a'])               # 행 기준으로 인덱싱
+print(sample_df.loc['a'])              # 행 기준으로 인덱싱
 print(sample_df.loc[['a','c','e']])
 print(sample_df.loc['a':'c'])
 
@@ -37,6 +37,6 @@ print(sample_df.loc['a':'c'])
 print(sample_df.iloc[[0,1,2]])
 print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
-print(sample_df.iloc[0:3, 2:4])         # 컬럼까지 동시에 인덱싱
+print(sample_df.iloc[0:3, 2:4])        # 컬럼까지 동시에 인덱싱
 
-print(sample_df.drop(['var_1','var_3'], axis=1))
+print(sample_df.drop(['var_1','var_3'], axis=1))     # 컬럼을 제거하려면 axis = 1
