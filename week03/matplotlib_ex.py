4773eabaf53c85769c2b21c212586e65424f7610
방경민 <kangmin2325@gmail.com>
2026-03-23 21:42:37 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -85,3 +85,8 @@ for i, value in enumerate(survived_counts):
 
   # 결측치 처리
   titanic = titanic.dropna(subset=['Age', 'Fare', 'Survived'])
+  print(titanic.info())
+
+  # 산점도 그래프 그리기
+  plt.figure(figsize=(12, 8))
+  scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanci[])
