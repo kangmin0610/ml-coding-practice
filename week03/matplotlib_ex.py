5bf678a9714c2aa3138148b22997e73fb528e2a5
방경민 <kangmin2325@gmail.com>
2026-03-23 23:26:09 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -357,4 +357,13 @@ axes[1].tick_params(axis='y', labelcolor='deeppink')
 axes[1].legend(loc='upper right')
 
 # 제목 설정
-fig.suptitle('Survival Analysis ')
+fig.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+fig.tight_layout()
+plt.savefig('Figure15.png')
+plt.close()
+
+## **하나의 서브플롯에 여러 그래프 그리기**
+
+# 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
+parch_counts =titanic.groupby('Parch')['Survived'].value_counts().unstack().fillna(0)
+
