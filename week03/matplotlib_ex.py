17114e2908c5b36278f9dbb26c0f0d6920829f5d
방경민 <kangmin2325@gmail.com>
2026-03-23 21:59:37 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -132,6 +132,12 @@ for i, value in enumerate(survived_counts):
   plt.title('Distribution of Ages on the Titanic')
   plt.grid(axis='y', linestyle='--', alpha=0.7)
   plt.savefig('Figure06.png')
-  plt.close
+  plt.close()
 
-  
+## 히트맵 : 두 변수의 상관 관계를 표시하기**
+
+# 결측치 처리
+titnaic = titanic.dropna(subset=['Age', 'Fare'])
+
+# 상관 행렬 계산
+correlation_matrix =
