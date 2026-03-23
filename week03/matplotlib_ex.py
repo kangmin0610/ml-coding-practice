f5dccb1b228bf3a83f856d65f44ebbd6a66c8236
방경민 <kangmin2325@gmail.com>
2026-03-23 22:47:08 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -248,4 +248,4 @@ print(fare_std)
 plt.figure(figsize=(10,6))
 
 # 에러바 생성
-plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt=)
+plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='o')
