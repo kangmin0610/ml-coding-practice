79b2d77c9f675af31a1bce2fea728e2978a4bca8
방경민 <kangmin2325@gmail.com>
2026-03-31 23:52:33 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -2,3 +2,23 @@
 
 # 시본 라이브러리 불러오기
 import seaborn as sns
+
+# **팁(tips) 데이터셋 불러오기**
+tips = sns.load_dataset('tips')
+print(tips.head())
+
+tips.info()
+
+# **범주형 변수 산점도 그래프**
+
+import matplotlib.pyplot as plt
+
+# figure에 2개의 서브 플롯을 생성
+fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+
+# stripplot( ) 그리기
+sns.stripplot(x='day', y='tip', hue='sex', data=tips, alpha=0.7, ax=ax1)
+
+# swarmplot( ) 그리기
