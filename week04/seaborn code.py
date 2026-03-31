3614a2ca5281ee406093939ea8c2eda6c37b80c3
방경민 <kangmin2325@gmail.com>
2026-03-31 21:25:31 +0900

    Updated Changes

--- week04/seaborn code.py
+++ week04/seaborn code.py
@@ -15,3 +15,8 @@ import matplotlib.pyplot as plt
 
 # figure에 2개의 서브 플롯을 생성
 fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+
+# stripplot( ) 그리기
+sns.stripplot(x='day', y='tip', hue='sex', data=tips, alpha=0.7, ax)
