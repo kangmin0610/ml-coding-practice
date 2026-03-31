471b17aa280448109fe9b683f15f890c7a765f76
방경민 <kangmin2325@gmail.com>
2026-04-01 00:20:40 +0900

    Updated Changes

---
+++ week04/data_preprocessing.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# 데이터 준비
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import tarfile
+import urllib.request
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            
