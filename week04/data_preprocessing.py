74849a074a5dc8d70bac256a9ed6d876e9be92a1
방경민 <kangmin2325@gmail.com>
2026-04-01 00:34:10 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -55,3 +55,13 @@ housing_num = housing.select_dtypes(include=[np.number])
 housing_num.head()
 
 imputer.fit(housing_num)
+
+print(imputer.statistics_)           # imputer 결과 값
+print(housing_num.median().values)   # 수동으로 계산한 중간값
+
+# 훈련 세트의 누락값을 imputer가 학습한 값으로 채우기
+X = imputer.transform(housing_num)
+
+imputer.feature_names_in_
+
+housing
