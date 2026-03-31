b974810bace0a2583d1e4320069042a9785f6077
방경민 <kangmin2325@gmail.com>
2026-03-31 21:29:01 +0900

    Updated Changes

--- week04/seaborn code.py
+++ week04/seaborn code.py
@@ -26,4 +26,12 @@ sns.swarmplot(x='day', y='tip', hue='sex', data=tips, palette='Set2', alpha=0.7,
 
 # 서브 플롯의 제목 설정
 ax1.set_title('Strip Plot of Tip by Day and Gender')
-ax2.set
+ax2.set_title('Swarm Plot of Tip by Day and Gender')
+plt.savefig('./week04/Seaborn_Figure01.jpg')
+
+# **빈도 그래프**
+# figure에  2개의 서브 플롯을 생성
+fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+
