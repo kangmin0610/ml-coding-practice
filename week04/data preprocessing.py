d3788d532437c377d21f189249075649c6bfaa16
방경민 <kangmin2325@gmail.com>
2026-03-31 22:36:02 +0900

    Updated Changes

--- week04/data preprocessing.py
+++ week04/data preprocessing.py
@@ -77,4 +77,7 @@ outlier_pred = isolation_forest.fit_predict(X)
 outlier_pred
 
 housing = housing.iloc[outlier_pred == 1]
-housing_labels = housing
+housing_labels = housing_labels.iloc[outlier_pred == 1]
+
+# 텍스트와 범주형 특서 다루기
+housing
