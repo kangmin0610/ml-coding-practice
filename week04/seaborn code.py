d0aee66f00f37352d60febb659f67f980191d756
방경민 <kangmin2325@gmail.com>
2026-03-31 21:24:01 +0900

    Updated Changes

---
+++ week04/seaborn code.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# 시본 라이브러리 불러오기
+import seaborn as sns 
+
+# **팁(tips) 데이터셋 불러오기**
+tips = sns.load_dataset('tips')
+print(tips.head())
+
+tips.info()
+
+# **범주형 변수 산점도 그래프**
+
+import matplotlib.pyplot as plt
+
+# figure에 2개의 서브 플롯을 생성
+fig = plt.figure(figsize=(15, 5))
