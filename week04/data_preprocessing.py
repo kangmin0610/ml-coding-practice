03935502608b6c3dc310b98d053c6e2cc21f78e3
방경민 <kangmin2325@gmail.com>
2026-04-02 18:25:22 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -4,4 +4,4 @@
 import numpy as np
 import pandas as pd
 
-housing = pd.read_csv('./week04/housing.csv')       # 오류 발생 시 
+housing = pd.read_csv('./week04/housing.csv')       # 오류 발생 시, ./housing.csv 파일로도 시도 
