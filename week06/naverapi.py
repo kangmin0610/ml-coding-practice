d169299e6f3553df3b34ad2e76a03b598422b656
방경민 <kangmin2325@gmail.com>
2026-04-14 18:15:16 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -35,3 +35,9 @@ def main():
 
     print("가져온 데이터 : %d 건" %(cnt))
     print('%s_naver_%s.json SAVED' % (srcText, node))
+
+    
+def getNaverSearch(node, srcText, page_start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" %
