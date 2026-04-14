842941f5557b34c94a5fb0e6469e0f621fbefd53
방경민 <kangmin2325@gmail.com>
2026-04-14 18:11:46 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -27,4 +27,6 @@ def main():
     
     print('전체 검색 : %d 건' %total)
 
-    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding= )
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding= 'utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
+                              ensure_ascil= False)
