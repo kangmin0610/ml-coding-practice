7d6ae647db7014a6e940f03d0700a9d034496e26
방경민 <kangmin2325@gmail.com>
2026-04-14 18:04:16 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -4,4 +4,9 @@ import datetime
 import json
 
 client_id = 'RqqpPopQZ0Z1eECHbMbT'
-client_secret = ''
+client_secret = 'oWHbdS9PTC'
+
+def main():
+
+    node = 'news'                                             # 
+    srcText = input('검색어를 입력하세요: ')
