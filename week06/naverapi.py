ac6bf05780eeb902275ae071b8a6234b906f1c01
방경민 <kangmin2325@gmail.com>
2026-04-14 18:25:46 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -70,4 +70,10 @@ def getRequestUrl(url):
 
 def getPostData(post, jsonResult, cnt):  #[CODE 3]
     title = post['title']
-    description
+    description = post['description']
+    org_link = post['originallink']
+    link = post['link']
+
+    # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... ) 
+    # %d  일 (예, 01, 02, 03, ...)
+    # %b 짧은 형식의 
