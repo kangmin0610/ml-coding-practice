0b9094aff6effd7028e3ad5120c069ebc7822d62
방경민 <kangmin2325@gmail.com>
2026-04-14 18:51:47 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -9,11 +9,11 @@ ServiceKey = "b4de855dccf0a6a121c9c1a3c6c572c4f47bf007517a6a5aa8ce476369a9c145"
 """### [CODE 0]"""
 
 def main():
-    jsonResult = []
-    result = []
+  jsonResult = []
+  result = []
 
-    print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
-    nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
-    nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
-    nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
-    ed_cd
+  print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
+  nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
+  nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
+  nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
+  ed_cd = "E" 		                     
