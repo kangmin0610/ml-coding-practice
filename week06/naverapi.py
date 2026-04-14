000b5e8f6c1452906cfd9838f2f7de77ce274456
방경민 <kangmin2325@gmail.com>
2026-04-14 18:05:46 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -10,4 +10,8 @@ def main():
 
     node = 'news'                                             # 크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
-    
+
+    cnt  = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
