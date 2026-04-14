d492763f2ed296d8bf30145a0d1b701ef40e0675
방경민 <kangmin2325@gmail.com>
2026-04-14 19:22:17 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -62,4 +62,20 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
   parameters = "?_type=json&serviceKey=" + ServiceKey       # 인증키
   parameters += "&YM=" + yyyymm
   parameters += "&NAT_CD=" + nat_cd
+  parameters += "&ED_CD" + ed_cd
+
+  url = service_url + parameters
+
+  responseDecode = getRequestUrl(url)                       #[CODE 1]
+
+  if (responseDecode == None):
+    return None
+  else:
+    return json.loads(responseDecode)
   
+"""### [CODE 1]"""
+
+def getRequestUrl(url):  #[CODE 1]
+  req = urllib.request.urlopen(req)
+  try:
+    response =
