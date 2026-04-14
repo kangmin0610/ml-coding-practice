bc56815a8532c76963a770e54bf9b3feb99ba8f3
방경민 <kangmin2325@gmail.com>
2026-04-14 18:48:46 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -5,3 +5,11 @@ import json
 import pandas as pd
 
 ServiceKey = "b4de855dccf0a6a121c9c1a3c6c572c4f47bf007517a6a5aa8ce476369a9c145"
+
+"""### [CODE 0]"""
+
+def main():
+    jsonResult = []
+    result = []
+
+    print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
