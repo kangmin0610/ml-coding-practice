4c65685c4f66797f46cf7c70cb17e0d1eda6c95e
방경민 <kangmin2325@gmail.com>
2026-04-28 20:54:11 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -58,3 +58,21 @@ dummy_clf.fit(X_train, y_train_5)
 print(any(dummy_clf.predict(X_train)))
 
 print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+
+# 오차 행렬
+from sklearn.model_selection import cross_val_predict
+
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
+
+y_train_perfect_predictions = y_train_5  # 완벽한 분류기일 경우
+print(confusion_matrix(y_train_5, y_train_perfect_predictions))
+
+# 정밀도와 재현율
+from sklearn.metrics import precision_score, recall_score
+
+print(precision_)
