b90a9c652c8e396b82ee202be6b49510bf165760
방경민 <kangmin2325@gmail.com>
2026-04-28 20:50:11 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -37,3 +37,24 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 # 이진 분류기 훈련
 y_train_5 = (y_train == '5')  # 5는 True고, 다른 숫자는 모두 False
+y_test_5 = (y_test == '5')
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
+
+# 성능 측정 - 교차 검증을 사용한 정확도 측정
+from sklearn.model_selection import cross_val_score
+
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+from sklearn.dummy import DummyClassifier
+
+dummy_clf = DummyClassifier()
+dummy_clf.fit(X_train, y_train_5)
+print(any(dummy_clf.predict(X_train)))
+
+print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
