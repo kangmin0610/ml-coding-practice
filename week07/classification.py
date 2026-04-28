c1c188e5bab08f3b59bde2c8b7ad5148d2cf0c9b
방경민 <kangmin2325@gmail.com>
2026-04-28 20:39:11 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 from sklearn.datasets import fetch_openml
 
+mnist = fetch_openml('mnist_784', as_frame=False)
+
+print(mnist.keys())  # data와 target만 사용
+
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape)
