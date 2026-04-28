9ac76c9260e37adb1c137513488b85f9e84bfc72
방경민 <kangmin2325@gmail.com>
2026-04-28 20:44:41 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -29,4 +29,9 @@ plt.figure(figsize=(9, 9))
 for idx, image_data in enumerate(X[:100]):
     plt.subplot(10, 10, idx + 1)
     plot_digit(image_data)
-plt.subplots_adjust(wspace=, hspace=)
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.show()
+
+# train/test 데이터셋 나누기
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
