a9024c4babb6207845d70dda95cba57598f53107
방경민 <kangmin2325@gmail.com>
2026-04-28 20:55:12 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -75,4 +75,6 @@ print(confusion_matrix(y_train_5, y_train_perfect_predictions))
 # 정밀도와 재현율
 from sklearn.metrics import precision_score, recall_score
 
-print(precision_)
+print(precision_score(y_train_5, y_train_pred))  # == 3530 / (687 + 3530)
+
+print(recall_score(y_train_5, y_train_pred))
