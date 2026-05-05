cf97b41d893e7a9fd3b0a70c7f6c52866b145946
방경민 <kangmin2325@gmail.com>
2026-05-05 13:03:13 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -136,4 +136,17 @@ sns.barplot(x=genres.values, y=genres.index, hue=genres.index, palette='RdGy')
 
 plt.title('Distribution of Genres for Movies and TV Shows on Netflix', fontsize=16)
 plt.xlabel('Count', fontsize=14)
-pl
+plt.ylabel('Genre', fontsize=14)
+plt.grid(axis='x')
+plt.show()
+
+netflix[netflix['title'].str.contains('Sankofa', na=False, case=False)]
+
+# 출력할 최대 행 수를 None으로 설정해서 모두 출력
+pd.set_option('display.max_rows', None)
+
+# 쉼표로 country 열의 값을 파이썬 리스트로 만들기
+netflix['country'] = netflix['country'].str.split(', ')
+netflix['country']
+
+#
