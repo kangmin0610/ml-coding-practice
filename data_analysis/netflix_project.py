c87b17281dbe3dfdf815acd564323b2673e0a112
방경민 <kangmin2325@gmail.com>
2026-05-05 12:32:12 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -23,4 +23,21 @@ netflix.info()
 for i in netflix.columns :
     missingValueRate = netflix[i].isna().sum() / len(netflix) * 100
     if missingValueRate > 0 :
-        print("{} null rate: {}")
+        print("{} null rate: {}%".format(i, round(missingValueRate ,2)))
+        
+# .fillna( ) : 결측치를 다른 값으로 대체하여 처리
+# 결측치 비율 : country(9.44%)
+netflix['country'] = netflix['country'].fillna('No Data')
+
+# .replace(np.nan, 'b') : 결측치를 문자열 바꾸기 함수를 통해 처리
+# 결측치 비율 : director(29.91%), cast(9.37%)
+netflix['director'] = netflix['director'].replace(np.nan, 'No Data')
+netflix['cast'] = netflix['cast'].replace(np.nan, 'No Data')
+
+# .dropna(axis = 0) : 결측치가 있는 행 전체 제거
+# 원본 객체를 수정하려면 inplace = True 옵션 추가
+# 결측치 비율 : data_added(0.11%), rating(0.05%), duration(0.03%)
+netflix.dropna(axis = 0, inplace=True)
+
+# .info() : 열에 대한 요역 정보 확인
+# 8807 rows(원본 데이터 행 개수) - 17 rows()
