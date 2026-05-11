2137dbc236cc2fbd38ad63a734f9c00d7d2c8913
방경민 <kangmin2325@gmail.com>
2026-05-11 18:59:05 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -9,4 +9,33 @@ from sklearn.svm import LinearSVC
 
 iris = load_iris(as_frame=True)
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
-y = (iris.target == 2)  # Iris 
+y = (iris.target == 2)  # Iris virginica
+
+svm_clf = make_pipeline(StandardScaler(),
+                        LinearSVC(C=1, dual=True, random_state=42))
+svm_clf.fit(X, y)
+
+X_new = [[5.5, 1.7], [5.0, 1.5]]
+
+print(svm_clf.predict(X_new))
+print(svm_clf.decision_function(X_new))
+
+# 비선형 SVM 분류
+
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import PolynomialFeatures
+
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+polynomial_svm_clf = make_pipeline(
+    PolynomialFeatures(degree=3),
+    StandardScaler(),
+    LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)    
+)
+polynomial_svm_clf.fit(X, y)
+
+# 다항식 커널
+
+from sklearn.svm import SVC
+
+poly_kernel_svm_clf = make_pipeline()
