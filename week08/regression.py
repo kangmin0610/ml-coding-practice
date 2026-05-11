0be285ae6946d0ca8bbaa0c9a506aa78cff42955
방경민 <kangmin2325@gmail.com>
2026-05-11 18:30:35 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -143,3 +143,10 @@ print(iris.data.head(3))
 print(iris.target.head(3))  # 샘플이 섞여 있지 않습니다.
 
 print(iris.target_names)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+X = iris.data[["petal width (cm)"]].values
+y = iris.target_names[iris.target] == 'virginica'
+X_train, X_test, y_train, y_test = train
