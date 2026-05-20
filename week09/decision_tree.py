785ba8d25f98f4a85f19547f76c24d04f45ab7f5
방경민 <kangmin2325@gmail.com>
2026-05-20 20:26:50 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -5,4 +5,5 @@ iris = load_iris(as_frame=True)
 X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y_iris = iris.target
 
-tree_clf = DecisionTreeClassifier(max_depth=2, random_state=)
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf.fit(X_iris, y_iris)
